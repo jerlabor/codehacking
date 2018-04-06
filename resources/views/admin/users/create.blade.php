852d3f74e9d9c2d88a1e69ae07a5c4044070d32d
John Edward R. Labor <john.dotaboy13@gmail.com>
2018-04-06 14:44:00 +0800

    create user form created

--- resources/views/admin/users/create.blade.php
+++ resources/views/admin/users/create.blade.php
@@ -3,5 +3,16 @@
 @section('content')
     <h1>Create User</h1>
 
+    {!! Form::open(['method'=>'POST','action'=>'AdminUsersController@store']) !!}
+
+        <div class="form-group">
+            {!! Form::label('name','Name:') !!}
+            {!! Form::text('name',null,['class'=>'form-control']) !!}
+        </div>
+        {!! Form::submit('Create User',['class'=>'btn btn-primary']) !!}
+        {{csrf_field()}}
+
+    {!! Form::close() !!}
+
 
     @endsection
